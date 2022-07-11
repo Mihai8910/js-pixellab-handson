b5a680b7d25db83efaed2a17bf847212d9d156ad
Mihai8910 <101422568+Mihai8910@users.noreply.github.com>
2022-07-11 22:00:52 +0300

    Curs js 11.07

---
+++ M01S03/ex04/app.js
@@ -0,0 +1,20 @@
+var myArray = [1, 2, 3, 4, 5, 6];
+
+console.warn(` Afiseaza numarul de pe pozitia 5 a arrauilui.
+`);
+
+console.log(myArray[5]);
+
+console.warn(`Afiseaza numarul 3 din array.
+`);
+console.log(myArray[2].toString());
+
+console.warn(`Afiseaza lungimea arraului.`);
+
+console.log(myArray.length.toString());
+
+console.warn(`Afiseaza suma numerelor de pe pozitia 0 si 4.`);
+
+console.log(myArray[0] + myArray[4]);
+
+console.log(myArray[1]);
