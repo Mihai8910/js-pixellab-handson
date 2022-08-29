821342467b785d09251e213f3ffd1f9742bea39c
Mihai8910 <101422568+Mihai8910@users.noreply.github.com>
2022-08-29 19:42:30 +0300

    1942

---
+++ M02S04/ex03/app.js
@@ -0,0 +1,9 @@
+const heroElement = document.querySelector('.hero');
+const constrols = document.querySelector('.controls');
+
+constrols.addEventListener('click', (event) => {
+  const button = event.target;
+
+  if (button.nodeName !== 'BUTTON') {
+  }
+});
